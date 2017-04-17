60b812ffb349deb5c07cdc8be530e51ecf37cdeb
Benjamin Goegge <bn@ggge.de>
2017-04-17 13:39:40 +0200

    completed tasks

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
+  var reportUrl = 'https://www.codeschool.com/users/bnggge.json';
+  var courses = [];
+  $.ajax({
+    url: reportUrl,
+    dataType: 'jsonp',
+    success: function(response) {
+      courses = response.courses.completed;
+      //console.log(response.courses.completed);
+      for (let course of courses) {
+        console.log(course.title);
+        $("#badges").append('<div class="course"><h3>'+course.title+'</h3><img src="'+course.badge+'"><a href="'+course.url+'" target="_blank" class="btn btn-primary">See Course</a></div>');
+      }
+    }
+  });
+
   // your code will go here
 
 });
